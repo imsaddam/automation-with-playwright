3baa99797d288c968f6410db92d15e0e3474c13e
imsaddam <nfo.saddamxm@gmail.com>
2026-01-16 23:03:33 +0100

    login system negative test added

--- login_system_automation.py
+++ login_system_automation.py
@@ -4,16 +4,26 @@ from tkinter.font import names
 
 from playwright.sync_api import Page, expect
 
-
-def test_loginIntoToSystem(page: Page):
+## Verifies that a user can log in successfully using valid credentials
+def test_login_successfully_with_valid_credentials(page: Page):
     page.goto("https://rahulshettyacademy.com/loginpagePractise/")
     page.get_by_label("Username:").fill("rahulshettyacademy")
     page.get_by_label("Password:").fill("learning")
  #   page.get_by_label("Admin").check()
     page.get_by_role("combobox").select_option("teach")
-    page.get_by_label("terms and conditions").check()
-    page.get_by_role("button", name="Sign In").click()
-    page.wait_for_timeout(1)
+    page.locator("#terms").check()
+    page.locator("#signInBtn").click()
+    expect(page.get_by_text("ProtoCommerce Home", exact=True)).to_be_visible()
+
+## // Verifies that a user cannot log in using invalid credentials
+def test_login_successfully_with_invalid_credentials(page: Page):
+    page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+    page.get_by_label("Username:").fill("rahulshettyacademy")
+    page.get_by_label("Password:").fill("learning1")
+    page.get_by_role("combobox").select_option("teach")
+    page.locator("#terms").check()
+    page.locator("#signInBtn").click()
+    expect(page.get_by_text("Incorrect username/password.", exact=True)).to_be_visible()
 
 
 
